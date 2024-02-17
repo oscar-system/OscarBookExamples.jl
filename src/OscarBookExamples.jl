2340e47481ba0e1bf5516462fd389a8751e30bf8
Benjamin Lorenz <lorenz@math.tu-berlin.de>
2024-02-17 20:23:36 +0100

    fixup2: remove stray '# output' even with newline

--- src/OscarBookExamples.jl
+++ src/OscarBookExamples.jl
@@ -355,7 +355,7 @@ function prepare_jlcon_content(content::AbstractString; remove_prefixes=true)
   result = content
   # Get rid of comments in the code
   result = replace(result, r"^#\D(?!output)\D.*$"m => "") # keep '# output'
-  result = replace(result, r"\n# output$" => "\n") # unless it is the last line
+  result = replace(result, r"\n# output\n?$" => "\n") # unless it is the last line
   # Get rid of empty lines with whitespaces
   result = replace(result, r"^\s*$"m => "")
   # Get rid of many empty lines
